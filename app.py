613eed4de84cc7b01357775c3c2997297de4bed6
mithulpiranav <pdmithulpiranav@gmail.com>
2026-04-15 20:31:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,7 @@ def dashboard():
                            graph=graph)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
